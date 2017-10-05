3218b8b2044a6438242f041529a091fd0f636f84
Unknown <jayasai.amerineni@gmail.com>
2017-10-05 20:26:17 +1100

    pending request activity

--- app/src/main/java/rogueone/rogueonemobliecomputing/TripActivity.java
+++ app/src/main/java/rogueone/rogueonemobliecomputing/TripActivity.java
@@ -9,20 +9,11 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Serializable;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import rogueone.rogueonemobliecomputing.Interfaces.APIClient;
 import rogueone.rogueonemobliecomputing.Interfaces.ServiceGenerator;
-import rogueone.rogueonemobliecomputing.Models.PendingRequest;
 
 public class TripActivity extends OptionsMenuActivity {
     ProgressDialog progressDialog;
-    APIClient client;
-    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,77 +51,5 @@ public class TripActivity extends OptionsMenuActivity {
     @Override
     public void showErrorToast(Throwable t){
         Toast.makeText(getApplicationContext(),t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
-    }
-    @Override
-    public boolean showFriends() {
-        startDialog();
-        Call<List<String>> call = client.getFriendList();
-        call.enqueue(new Callback<List<String>>() {
-            @Override
-            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
-                closeDialog();
-                Intent intent =new Intent(getApplicationContext(),FriendsActivity.class);
-                intent.putExtra("friendList",(Serializable)response.body());
-                intent.putExtra("token",token);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-
-            @Override
-            public void onFailure(Call<List<String>> call, Throwable t) {
-                closeDialog();
-                showErrorToast(t);
-            }
-        });
-        return true;
-    }
-    @Override
-    public boolean showPending() {
-        startDialog();
-        Call<List<PendingRequest>> call = client.getPendingRequests();
-        call.enqueue(new Callback<List<PendingRequest>>() {
-            @Override
-            public void onResponse(Call<List<PendingRequest>> call, Response<List<PendingRequest>> response) {
-                closeDialog();
-                Intent intent =new Intent(getApplicationContext(),PendingActivity.class);
-                intent.putExtra("token",token);
-                intent.putExtra("requestList",(Serializable)response.body());
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-            @Override
-            public void onFailure(Call<List<PendingRequest>> call, Throwable t) {
-                closeDialog();
-                showErrorToast(t);
-            }
-        });
-        return true;
-    }
-    @Override
-    public boolean connectWithUsers() {
-        startDialog();
-        Call<List<String>> call = client.getAppUsers();
-        call.enqueue(new Callback<List<String>>() {
-            @Override
-            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
-                closeDialog();
-                Intent intent = new Intent(getApplicationContext(),ConnectActivity.class);
-                List<String> users = response.body();
-                intent.putExtra("userList",(Serializable)users);
-                intent.putExtra("token",token);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-
-            @Override
-            public void onFailure(Call<List<String>> call, Throwable t) {
-                closeDialog();
-                showErrorToast(t);
-            }
-        });
-        return true;
     }
 }
