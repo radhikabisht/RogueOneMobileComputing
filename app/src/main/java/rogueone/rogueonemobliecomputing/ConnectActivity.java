24bb060a86eac5496f527a7e3796ab07cbfd60f7
Harshul Upadhyay <harshulmu91@gmail.com>
2017-10-05 20:20:31 +1100

    consolidated menu options

---
+++ app/src/main/java/rogueone/rogueonemobliecomputing/ConnectActivity.java
@@ -0,0 +1,109 @@
+package rogueone.rogueonemobliecomputing;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import rogueone.rogueonemobliecomputing.Interfaces.APIClient;
+import rogueone.rogueonemobliecomputing.Interfaces.ServiceGenerator;
+
+public class ConnectActivity extends OptionsMenuActivity {
+    ProgressDialog progressDialog;
+    @BindView(R.id.send_request)
+    Button _sendRequest;
+    @BindView(R.id.search_users)
+    AutoCompleteTextView _search_users;
+    public OnClickListener requestListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startDialog();
+            String acceptor = _search_users.getText().toString();
+            Call<ResponseBody> call = client.SendRequest(acceptor);
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if(response.isSuccessful()){
+                        closeDialog();
+                        Toast.makeText(getApplicationContext(),"Request Sent Succesfully",Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class).putExtra("token",token));
+                    }else{
+                        closeDialog();
+                            showErrorToast(new Throwable("Request already sent or the action is not allowed"));
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    closeDialog();
+                    showErrorToast(t);
+                }
+            });
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+        ButterKnife.bind(this);
+        try{
+            token = getIntent().getStringExtra("token");
+        }catch(Exception e){
+            Log.e(android.support.compat.BuildConfig.APPLICATION_ID,e.getMessage());
+        }
+        if(token==null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+        client = ServiceGenerator
+                .createService(APIClient.class,token,getBaseContext());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbarMain);
+        setTitle("LODI");
+        setSupportActionBar(toolbar);
+        TextView mTitle = (TextView) toolbar.getChildAt(0);
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/Lobster-Regular.ttf");
+        mTitle.setTypeface(tf);
+        Intent intent = getIntent();
+        List<String> users = (List<String>)intent.getSerializableExtra("userList");
+        progressDialog = new ProgressDialog(ConnectActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, users);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.search_users);
+        textView.setAdapter(adapter);
+        _sendRequest.setOnClickListener(requestListener);
+    }
+
+    @Override
+    public void startDialog(){
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+    }
+    @Override
+    public void closeDialog(){
+        progressDialog.dismiss();
+    }
+    @Override
+    public void showErrorToast(Throwable t){
+        Toast.makeText(getApplicationContext(),t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+    }
+}
